d32ee0d44a0c1868139b5cc0fc79326f7fa2fb46
AkashSShrivastava <sas5198sas@gmail.com>
2023-07-30 01:43:51 +0530

    Final commit

--- src/main/java/com/gold/user/service/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gold.user.service.repositories;
-
-import com.gold.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
